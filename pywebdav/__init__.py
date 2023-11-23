a0e2de3d37aa10a7ffd8e3ccd951863c3ff79f73
vanzhiganov <vanzhiganov@ya.ru>
2023-11-23 19:11:28 +0300

    fix deprecated import

--- pywebdav/__init__.py
+++ pywebdav/__init__.py
@@ -1,5 +1,5 @@
 """WebDAV library including a standalone server for python 3"""
-__version__ = '0.9.14'
+__version__ = '0.9.15'
 __author__ = 'Andrew Leech (previously Simon Pamies)'
 __email__ = 'andrew@alelec.net'
 __license__ = 'LGPL v2'
